a96c954b779d6a0c2a02c85d5fd5ddf08d52aa6d
John Cain <cain.johnp@gmail.com>
2014-05-27 16:36:25 -0400

    added my_solution for code combat challenge

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,11 +1,51 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
- 
+ // Keep Tharin Alive
+ //move right 1x
+ //ATTACK
+
+ //Grab the health potion
+ //move down 1x
+ //move up 1x
+
+ //Kill Munchkin 2
+ //move right 1x
+ //ATTACK
+
+ //Lure the Orge
+ //move right 3x
+ //move left 3x
+
+ //Escape to the right
+ //move right 3x
+
+ //Remove the orge
+//move down 2x
+//say "hey there!"
+//move up 2x
+//say attack
+
+//taunt the orge four times
+//insult the orge four times
+
+//Send in the troops
+//tell the soldiers to follow Tharin
+//move closer to the ogres
+//say attack
+//move tharin away from soldiers
+
+ //do what comes naturally
+
+ //fire on one squadron
+ //adjust aim, fire again
+ //move artillery
+ //fire on second squadron
+ //adjust aim, fire again
+ //say "here we go boys!"
  
  
  
@@ -14,3 +54,16 @@
  
 // Reflection:
 // Write your reflection here.
+
+// What is this referring to? Think programming-wise rather than in the terms of the game. 
+// "this" is referring to the object that the methods we called in the game were nested as properties
+
+// What does the () do in JavaScript? The () offers arguments that can be applied when calling a method. Empty parentheses signify that no arguments apply to the method being called.
+
+// What is the point of the semicolons? Semicolons separate statements within a js code. Semicolons are required whenever two statements are used on the line, but 
+// but are not required if a line break separate two statements. semicolons should not be used after curly brackets or round brackets of an if, for while, or 
+// switch statement. Apart from these exceptions, code is generally easier to read and parse when semicolons are used inbetween statements. 
+
+
+
+
